121204d1595e43de3271c3b1d98025649be7029d
MariaBetancur <mabo6241@gmail.com>
2023-05-26 17:57:35 -0500

    v1.2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let timerId = null;
+
+startButton.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+});
