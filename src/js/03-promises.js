e1dcf1e6bd4dabe6843989323b426be14a90a8b7
MariaBetancur <mabo6241@gmail.com>
2023-05-29 21:50:58 -0500

    v1.4

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    } else {
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    }
+  });
+}
+
+function createPromises(event) {
+  event.preventDefault();
+  const form = event.target;
+  const firstDelay = parseInt(form.delay.value);
+  const step = parseInt(form.step.value);
+  const amount = parseInt(form.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const delay = firstDelay + i * step;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+const form = document.querySelector('.form');
+form.addEventListener('submit', createPromises);
